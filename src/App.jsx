f1b7f8545b133d96ac17e1e8080b8a750600aec8
diogo-gb Iwamoto <diogoiwamoto7999@gmail.com>
2023-10-16 19:57:40 +0000

    Aula react inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+  <div className="centraliza">
+
+    <div style={{height: '8rem'}}>
+     React
+     </div>
+
+     <div style={{height: '8rem'}}>
+     React 2
+     </div>
+
+  </div>
+  );
+}
+
+export default App;
